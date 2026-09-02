06a324f568f055e2865a2b643a833a17a24fd204
muhammadjonmirzajonov12-collab <muhammadjonmirzajonov12@gmail.com>
2026-09-02 13:49:28 +0500

    Ishga_Tushirish.bat qoshildi

---
+++ Ishga_Tushirish.bat
@@ -0,0 +1,9 @@
+@echo off
+title Telegram Builder Bot 24/7
+chcp 65001 > nul
+set PYTHONIOENCODING=utf-8
+echo ====================================================
+echo      Telegram Builder Bot 24/7 Ishga Tushmoqda...
+echo ====================================================
+python run_forever.py
+pause
